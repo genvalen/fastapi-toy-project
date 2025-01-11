7633b2e68c84db7d7e1ff78b0bb050344ab34fd7
genvalen <genvalen@protonmail.com>
2025-01-11 02:37:21 -0500

    Complete ORM CRUD endpoints.

--- app/app.py
+++ app/app.py
@@ -68,14 +68,22 @@ async def get_posts(db: Session = Depends(get_db)):
     return {"data": posts}
 
 @app.get("/posts/{id}")
-async def get_post(id: int): #path parameters will automatically be returned as string unless otherwise indicated
-    cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
-    post = cursor.fetchone()
-    if not post:
-        detail = f"post with id {id} not found."
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail=detail)
+async def get_post(id: int, db : Session = Depends(get_db)): #path parameters will automatically be returned as string unless otherwise indicated
+    # cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
+    # post = cursor.fetchone()
+    # if not post:
+    #     detail = f"post with id {id} not found."
+    #     raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail=detail)
 
-    return (f"post {id}:", post)
+    # return (f"post {id}:", post)
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        detail = f"Post with id {id} was not found.",
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
+    db.commit()
+    db.refresh(post)
+
+    return {"post": post}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 async def create_posts(post: Post, db: Session = Depends(get_db)):
@@ -93,26 +101,46 @@ async def create_posts(post: Post, db: Session = Depends(get_db)):
 # async def get_latest_post():
 #     return my_posts[-1]
 
-
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-    if not deleted_post:
-        detail = f"message: post with id {id} not found."
+async def delete_post(id: int, db : Session = Depends(get_db)):
+    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    # if not deleted_post:
+    #     detail = f"message: post with id {id} not found."
+    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
+
+    # return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+
+    if post_query.first() == None:
+        detail = f"Post with id {id} was not found.",
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
 
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    post_query.delete(synchronize_session=False)
+    db.commit()
 
+    return Response(status_code=status.HTTP_404_NOT_FOUND)
 
 @app.put("/posts/{id}")
-async def update_post(id: int, post: Post):
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id),))
-    updated_post = cursor.fetchone()
-    conn.commit()
-    if not updated_post:
-        detail = f"message: post with id {id} was not found."
+async def update_post(id: int, post: Post, db: Session = Depends(get_db)):
+    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id),))
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+    # if not updated_post:
+    #     detail = f"message: post with id {id} was not found."
+    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
+
+    # return ({"updated post": updated_post})
+
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post_payload = post_query.first()
+
+    if post_query.first() == None:
+        detail = f"Post with id {id} was not found.",
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
 
-    return ({"updated post": updated_post})
+    post_query.update(post.model_dump(), synchronize_session=False)
+    db.commit()
+    return {"post:": post_query.first()}
